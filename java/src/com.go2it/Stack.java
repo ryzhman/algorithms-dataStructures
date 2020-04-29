b003a71a2c9e055639e4eb96d13cb013f31aafac
Oleksandr Ryzhkov <oryzhkov@paymentus.com>
2020-04-28 23:05:37 -0400

    Sample Stack implementation

---
+++ java/src/com.go2it/Stack.java
@@ -0,0 +1,42 @@
+public class Stack {
+    private int[] elems;
+    private int currentSize = 0;
+    private int maxSize;
+
+    public Stack(int[] elems) {
+        this.elems = elems;
+        this.maxSize = elems.length;
+    }
+
+    public Stack(int size) {
+        this.elems = new int[size];
+        this.maxSize = size;
+    }
+
+    public void push(int i) {
+        if ((currentSize + 1) > maxSize) {
+            return;
+        }
+        elems[currentSize++] = i;
+    }
+
+    public int pop() {
+        if (currentSize <= 0) {
+            throw new RuntimeException("Stack is empty");
+        }
+        int itemToReturn = elems[--currentSize];
+        elems[currentSize] = 0;
+        return itemToReturn;
+    }
+
+
+    public static void main(String[] args) {
+        final Stack stack = new Stack(3);
+        stack.push(3);
+        stack.push(6);
+        stack.push(1);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+    }
+}
