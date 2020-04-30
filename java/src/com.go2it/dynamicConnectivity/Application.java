677cc1fe897d7e4b2e3c6a78f3b0e11be49f5332
Oleksandr Ryzhkov <oryzhkov@paymentus.com>
2020-04-30 09:05:16 -0400

    Dynamic connectivity: PathCompression with Weightened quick union

--- java/src/com.go2it/dynamicConnectivity/Application.java
+++ java/src/com.go2it/dynamicConnectivity/Application.java
@@ -8,21 +8,16 @@ import java.nio.file.Paths;
 public class Application {
     public static void main(String[] args) throws IOException {
         final DataStructure dataStructure;
-        try(BufferedReader bufferedReader = Files.newBufferedReader(Paths.get("C:\\Users\\oryzhkov\\Documents\\git\\algorithms\\java\\src\\resources\\tinyUF.txt"))) {
+        try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get("C:\\Users\\oryzhkov\\Documents\\git\\algorithms\\java\\src\\resources\\tinyUF.txt"))) {
 //            dataStructure = initQuickFindTree(bufferedReader);
 //            dataStructure = initQuickUnionTree(bufferedReader);
-            dataStructure = initWeightenedQuickUnionTree(bufferedReader);
+//            dataStructure = initWeightenedQuickUnionTree(bufferedReader);
+            dataStructure = initPathCompressionWeightenedQuickUnionTree(bufferedReader);
         }
 
         System.out.format("Shared component for %d and %d: ", 1, 5);
         System.out.println(dataStructure.findComponent(1, 5));
         System.out.println();
-//        System.out.format("%d and %d are in the same component: %b", 3, 4, dataStructure.areConnected(3, 4));
-//        System.out.println();
-//        System.out.format("%d and %d are in the same component: %b", 1, 5, dataStructure.areConnected(1, 5));
-//        System.out.println();
-//        System.out.format("%d and %d are in the same component: %b", 7, 5, dataStructure.areConnected(7, 5));
-//        System.out.println();
     }
 
     private static DataStructure initQuickFindTree(BufferedReader bufferedReader) throws IOException {
@@ -60,6 +55,22 @@ public class Application {
     private static DataStructure initWeightenedQuickUnionTree(BufferedReader bufferedReader) throws IOException {
         int numberOfElems = Integer.parseInt(bufferedReader.readLine());
         WeightenedQuickUnion weightenedQuickUnion = new WeightenedQuickUnion(numberOfElems);
+        String line = null;
+        while ((line = bufferedReader.readLine()) != null) {
+            final String[] nodes = line.split(" ");
+            final Integer i1 = Integer.valueOf(nodes[0]);
+            final Integer i2 = Integer.valueOf(nodes[1]);
+            if (!weightenedQuickUnion.areConnected(i1, i2)) {
+                weightenedQuickUnion.union(i1, i2);
+//                    System.out.println(i1 + " - " + i2);
+            }
+        }
+        return weightenedQuickUnion;
+    }
+
+    private static DataStructure initPathCompressionWeightenedQuickUnionTree(BufferedReader bufferedReader) throws IOException {
+        int numberOfElems = Integer.parseInt(bufferedReader.readLine());
+        PathCompressionWeightenedQuickUnion weightenedQuickUnion = new PathCompressionWeightenedQuickUnion(numberOfElems);
         String line = null;
         while ((line = bufferedReader.readLine()) != null) {
             final String[] nodes = line.split(" ");
