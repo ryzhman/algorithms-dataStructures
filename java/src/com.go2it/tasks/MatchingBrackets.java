1a443f71bb9befe3e2412f73e0682a13379353a9
Oleksandr Ryzhkov <oryzhkov@paymentus.com>
2020-05-07 08:54:32 -0400

    Matching brackets implementation

---
+++ java/src/com.go2it/tasks/MatchingBrackets.java
@@ -0,0 +1,78 @@
+package tasks;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MatchingBrackets {
+    private class Stack {
+        private char[] stack;
+        private int currentSize = 0;
+
+        public Stack() {
+            this.stack = new char[10];
+        }
+
+        public void push(char c) {
+            stack[currentSize++] = c;
+        }
+
+        public char pop() {
+            char c = stack[--currentSize];
+            stack[currentSize] = '0';
+            return c;
+        }
+    }
+
+    public boolean areAllBracketsClosed(String sequence) {
+        char[] chars = sequence.toCharArray();
+        Stack stack = new Stack();
+        Map<Character, Character> bracketsMap = prepareBracketsMap();
+        for (int i = 0; i < chars.length; i++) {
+            char currentBracket = chars[i];
+            //opening bracket
+            if (currentBracket == '(' || currentBracket == '{' || currentBracket == '[') {
+                stack.push(currentBracket);
+            } else {
+                //closing bracket
+                char lastStackedBracket = stack.pop();
+                //if the brackets don't match, fail
+                if (bracketsMap.get(lastStackedBracket) != currentBracket) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public char findMissingBracket(String sequence) {
+        char[] chars = sequence.toCharArray();
+        Stack stack = new Stack();
+        Map<Character, Character> bracketsMap = prepareBracketsMap();
+        for (int i = 0; i < chars.length; i++) {
+            char currentBracket = chars[i];
+            //opening bracket
+            if (currentBracket == '(' || currentBracket == '{' || currentBracket == '[') {
+                stack.push(currentBracket);
+            } else {
+                //closing bracket
+                char lastStackedBracket = stack.pop();
+                //if the brackets don't match, fail
+                if (bracketsMap.get(lastStackedBracket) != currentBracket) {
+                    return bracketsMap.get(lastStackedBracket);
+                }
+            }
+        }
+        return ' ';
+    }
+
+    private Map<Character, Character> prepareBracketsMap() {
+        Map<Character, Character> map = new HashMap<>();
+        map.put('{', '}');
+        map.put('(', ')');
+        map.put('[', ']');
+        return map;
+
+    }
+
+
+}
---
+++ java/test/com.go2it/tasks/MatchingBracketsTest.java
@@ -0,0 +1,35 @@
+package tasks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MatchingBracketsTest {
+    private final String VALID_SEQUENCE = "{([{}])}";
+    private final String INVALID_SEQUENCE = "{([{}]}";
+    @Test
+    public void areAllBracketsClosedSuccess() {
+        MatchingBrackets matchingBrackets = new MatchingBrackets();
+        assertTrue(matchingBrackets.areAllBracketsClosed(VALID_SEQUENCE));
+    }
+
+    @Test
+    public void areAllBracketsClosedFailed() {
+        MatchingBrackets matchingBrackets = new MatchingBrackets();
+        assertFalse(matchingBrackets.areAllBracketsClosed(INVALID_SEQUENCE));
+    }
+
+    @Test
+    public void findMissingBracketFailed() {
+        MatchingBrackets matchingBrackets = new MatchingBrackets();
+        assertEquals(')', matchingBrackets.findMissingBracket(INVALID_SEQUENCE));
+    }
+
+
+    @Test
+    public void findMissingBracketSuccess() {
+        MatchingBrackets matchingBrackets = new MatchingBrackets();
+        assertEquals(' ', matchingBrackets.findMissingBracket(VALID_SEQUENCE));
+    }
+
+}
