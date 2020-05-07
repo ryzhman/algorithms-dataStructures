5f8ee7e24edb567e71e12becacaaef70b1cbc1f5
Oleksandr Ryzhkov <oryzhkov@paymentus.com>
2020-05-07 08:23:37 -0400

    Shuffling implementation

---
+++ java/src/com.go2it/sorting/Shuffling.java
@@ -0,0 +1,15 @@
+package sorting;
+
+import java.util.Random;
+
+public class Shuffling {
+
+    public static Comparable[] shuffle(Comparable[] arr) {
+        Random random = new Random(arr.length -1);
+        for (int i = 0; i < arr.length; i++) {
+            SortingUtils.exchange(arr, random.nextInt(10), i);
+        }
+
+        return arr;
+    }
+}
---
+++ java/test/com.go2it/sorting/ShufflingTest.java
@@ -0,0 +1,30 @@
+package sorting;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ShufflingTest {
+    @Test
+    public void testShuffle() {
+        Comparable[] arr = new Comparable[10];
+        arr[0] = 99;
+        arr[1] = -2;
+        arr[2] = 123;
+        arr[3] = 12;
+        arr[4] = 38;
+        arr[5] = 72;
+        arr[6] = 2;
+        arr[7] = 0;
+        arr[8] = -1;
+        arr[9] = -11;
+        SelectionSort.sort(arr);
+
+        assertTrue(SortingUtils.isSorted(arr));
+
+        Shuffling.shuffle(arr);
+        assertFalse(SortingUtils.isSorted(arr));
+    }
+
+}
